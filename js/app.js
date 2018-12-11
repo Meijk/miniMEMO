1926447479b8feab47651b2a7854715e8e4eb44f
meijk <michael.schick@posteo.de>
2018-12-11 16:47:43 +1000

    Attach event listener to .card

--- js/app.js
+++ js/app.js
@@ -56,3 +56,7 @@ createNewDeck();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+document.querySelector('.deck').addEventListener('click', function (evt) {
+	
+})
+
